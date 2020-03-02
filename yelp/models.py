91b771816f7f88e3a32b3ee879d687d030e6642d
ProdigyX6217 <dominic.holder.15@cityas.org>
2020-03-02 14:14:11 -0800

    Created App, Used API Key, Mapped URL Patterns

--- yelp/models.py
+++ yelp/models.py
@@ -2,9 +2,10 @@ from django.db import models
 
 class Business(models.Model):
     name = models.CharField(max_length=100)
+    rating = models.DecimalField
     price = models.IntegerField
     id = models.BigIntegerField
-    product_photos = models.ImageField((upload_to=None, height_field=None, width_field=None, max_length=100)
+    # product_photos = models.ImageField((upload_to=None, height_field=None, width_field=None, max_length=100)
     delivery = models.BooleanField
     reservations = models.BooleanField
     
--- yelp/urls.py
+++ yelp/urls.py
@@ -1,21 +1,50 @@
 from django.urls import path
 from yelp import views
+import requests
+import json
 
 app_name = 'yelp'
 
-api_key='LS6f-xpVxlngO2qWn-j724FR0LJv8PztM4Xd9HDcOmj7k6RTxY_BVjIiPuZJ5tq4OMYK-qx9Ls-mAQ9-u6547gvHqEYopMA1MR31VUcooDHaPjsUDSsRabQ1oLtZXnYx'
-headers = {'Authorization': 'Bearer %s' % api_key}
+
+headers = {'Authorization': f'Bearer {api_key}'}
+
 
 url='https://api.yelp.com/v3/businesses/search'
+
+# In the params dictionary, term takes string values.
+params = {'term':'seafood','location':'San Francisco'}
+
+# Making a get request to the API.
+req=requests.get(url, params=params, headers=headers)
+
+# Convert the data into a JSON object.
+parsed = json.loads(req.text)
+ 
+# proceed only if the status code is 200
+print(f'The status code is {req.status_code}')
+
+'''
 url='https://api.yelp.com/v3/businesses/search/phone'
+
+params = {'phone':'+14159083801'}
+
+
 url='https://api.yelp.com/v3/businesses/matches'
+
+
+id="E8RJkjfdcwgtyoPMjQ_Olg"
+url=f'https://api.yelp.com/v3/businesses/{id}'
+
+
+url=f'https://api.yelp.com/v3/businesses/{id}/reviews'
+'''
 
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('/businesses/search', views.index, name='index'),
-    path('/businesses/search/phone', views.index, name='index'),
-    path('/businesses/{id}', views.index, name='index'),
-    path('/businesses/{id}/reviews', views.index, name='index'),
+    # path('/businesses/search/phone', views.index, name='index'),
+    # path('/businesses/{id}', views.index, name='index'),
+    # path('/businesses/{id}/reviews', views.index, name='index'),
 
 ]
